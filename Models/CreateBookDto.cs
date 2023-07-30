bb3860a726db045296b4f3cb4b0b6c2b41015f51
Ali Salmeh <alisalmeh18@gmail.com>
2023-07-30 14:03:27 +0330

    add length and range validation into createBookDto

--- Models/CreateBookDto.cs
+++ Models/CreateBookDto.cs
@@ -10,7 +10,11 @@ namespace AliBookStoreApi.Models
     {
         [Required(ErrorMessage = "لطفا عنوان کتاب را وارد کنید")]
         public string Title { get; set; }
+
+        [MaxLength(30)]
         public string Description { get; set; }
+
+        [Range(1000, 1000000)]
         public int Price { get; set; }
     }
 }
