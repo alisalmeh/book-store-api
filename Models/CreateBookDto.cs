af2c672d8036b17836bfd8a831011a17db72a128
Ali Salmeh <alisalmeh18@gmail.com>
2023-07-30 16:26:31 +0330

    add "BanKeywordAttribute" (a cutom validation)

--- Models/CreateBookDto.cs
+++ Models/CreateBookDto.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using AliBookStoreApi.Validations;
 
 namespace AliBookStoreApi.Models
 {
     public class CreateBookDto
     {
         [Required(ErrorMessage = "لطفا عنوان کتاب را وارد کنید")]
+        [BanKeyword]
         public string Title { get; set; }
 
         [MaxLength(30, ErrorMessage = "طول توضیحات بیشتر از 30 کرکتر نمیتواند باشد")]
@@ -17,6 +19,6 @@ namespace AliBookStoreApi.Models
         [Range(1000, 1000000, ErrorMessage = "مبلغ را بین 1000 تا 1000000 وارد کنید")]
         public int Price { get; set; }
 
-       
+
     }
 }
