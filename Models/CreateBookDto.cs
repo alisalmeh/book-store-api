773b36c69a1d0435b320cebf608f54f791515f80
Ali Salmeh <alisalmeh18@gmail.com>
2023-07-30 13:51:26 +0330

    refactor:add title validation with data annotation

--- Models/CreateBookDto.cs
+++ Models/CreateBookDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,7 @@ namespace AliBookStoreApi.Models
 {
     public class CreateBookDto
     {
+        [Required(ErrorMessage = "لطفا عنوان کتاب را وارد کنید")]
         public string Title { get; set; }
         public string Description { get; set; }
         public int Price { get; set; }
