9c12aa969736df30bc3478eb7a1d408ae8746611
Ali Salmeh <alisalmeh18@gmail.com>
2023-08-12 13:58:16 +0330

    add new entities and change Id to BId

--- Data/Book.cs
+++ Data/Book.cs
@@ -7,9 +7,11 @@ namespace AliBookStoreApi.Data
 {
     public class Book
     {
-        public int Id { get; set; }
+        public int BId { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
+        public string Author { get; set; }
+        public string Publisher { get; set; }
         public int Price { get; set; }
     }
 }
