e13e99bcfe4bb8f8760c5bf09e7cbab823365ca5
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-30 15:08:20 +0330

    chore: deleted the models and entities which were not needed.

--- Data/BookStoreContext.cs
+++ Data/BookStoreContext.cs
@@ -15,35 +15,15 @@ namespace AliBookStoreApi.Data
         }
 
         public DbSet<Book> Books { get; set; }
-        public DbSet<Author> Authors { get; set; }
         public DbSet<Category> Categories { get; set; }
-        public DbSet<Customer> Customers { get; set; }
-        public DbSet<FullAddress> FullAddresses { get; set; }
-        public DbSet<Publisher> Publishers { get; set; }
-        public DbSet<Translator> Translators { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<Book>()
                 .HasKey(x => x.BookId);
 
-            modelBuilder.Entity<Author>()
-                .HasKey(x => x.AuthorId);
-
             modelBuilder.Entity<Category>()
                 .HasKey(x => x.CategoryId);
-
-            modelBuilder.Entity<Customer>()
-                .HasKey(x => x.CusId);
-
-            modelBuilder.Entity<FullAddress>()
-                .HasKey(x => x.AddressId);
-
-            modelBuilder.Entity<Translator>()
-                .HasKey(x => x.TranslatorId);
-
-            modelBuilder.Entity<Publisher>()
-                .HasKey(x => x.PublisherId);
         }
     }
 }
