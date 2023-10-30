3009d0c36d1e40023f4e54461aa5dc9cdf110f5a
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-30 16:02:17 +0330

    style: rename books to book

--- Repositories/BookRepository.cs
+++ Repositories/BookRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AliBookStoreApi.Data;
+using AliBookStoreApi.Interfaces;
 using AliBookStoreApi.Models;
 using Microsoft.EntityFrameworkCore;
 
