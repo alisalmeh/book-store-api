76745ddca1cbeecd841548d3e93d0951d0c48741
Ali Salmeh <alisalmeh18@gmail.com>
2023-10-31 17:38:10 +0330

    refactor: changed Category entities

--- Data/Category.cs
+++ Data/Category.cs
@@ -7,8 +7,8 @@ namespace AliBookStoreApi.Data
 {
     public class Category
     {
-        public int CategoryId { get; set; }
+        public int Id { get; set; }
         public string Name { get; set; }
-        public DateTime LastUpdate { get; set; }
+        public string Description { get; set; }
     }
 }
