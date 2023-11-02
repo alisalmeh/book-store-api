7d790fe49da9e045975d4cbfe561cc464677c177
Ali Salmeh <alisalmeh18@gmail.com>
2023-11-02 15:27:36 +0330

    feat: add Author property

--- Models/BookDetailsDto.cs
+++ Models/BookDetailsDto.cs
@@ -9,6 +9,7 @@ namespace AliBookStoreApi.Models
     {
         public int Id { get; set; }
         public string Title { get; set; }
+        public string Author { get; set; }
         public string Description { get; set; }
         public float Price { get; set; }
     }
