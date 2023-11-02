bada4d7ddf44b493dc54806461b57e2263e7347e
Ali Salmeh <alisalmeh18@gmail.com>
2023-11-02 13:12:02 +0330

    refactor: add "ChangePriceDataType" migration

---
+++ Migrations/20231102091148_ChangePriceDataType.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AliBookStoreApi.Migrations
+{
+    public partial class ChangePriceDataType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<float>(
+                name: "Price",
+                table: "Books",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Price",
+                table: "Books",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(float));
+        }
+    }
+}
