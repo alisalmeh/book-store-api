b43cdb92ab2a3562198d58d66c5c8c6fe7a5d9f8
Ali Salmeh <alisalmeh18@gmail.com>
2023-11-02 15:27:10 +0330

    add CategoryController

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AliBookStoreApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ILogger<CategoryController> _logger;
+
+        public CategoryController(ILogger<CategoryController> logger)
+        {
+            _logger = logger;       
+        }
+    }
+}
