252bd2ecd3aabd2890867845613c8ff2a052418b
Ali Salmeh <alisalmeh18@gmail.com>
2023-11-08 12:26:32 +0330

    feat: apply validations to CreateCategoryDto

--- Models/CreateCategoryDto.cs
+++ Models/CreateCategoryDto.cs
@@ -1,13 +1,19 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using AliBookStoreApi.Validations;
 
 namespace AliBookStoreApi.Models
 {
     public class CreateCategoryDto
     {
+        [Required(ErrorMessage = "Name is required")]
+        [BanKeyword]
         public string Name { get; set; }
+
+        [MaxLength(500, ErrorMessage = "500 character limitation")]
         public string Description { get; set; }
     }
 }
