91ed442f0141711357d5e17571d72e7cd81df860
Ali Salmeh <alisalmeh18@gmail.com>
2023-11-13 16:09:15 +0330

    feat: add login dto

--- Models/Account/LoginDto.cs
+++ Models/Account/LoginDto.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,6 +8,11 @@ namespace AliBookStoreApi.Models
 {
     public class LoginDto
     {
-        
+        [Required]
+        [EmailAddress]
+        public string UserName { get; set; }
+
+        [Required]
+        public string Password { get; set; }
     }
 }
