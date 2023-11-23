da31fdb71514989487ace261110b1c24383eec15
Ali Salmeh <alisalmeh18@gmail.com>
2023-11-23 11:47:29 +0330

    feat: add swagger config for bearer authentication

--- Startup.cs
+++ Startup.cs
@@ -31,6 +31,32 @@ namespace AliBookStoreApi
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+                {
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer",
+                    BearerFormat = "JWT",
+                    In = ParameterLocation.Header,
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                });
+
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        new string[] {}
+                        // new List<string> ()
+                    }
+                });
             });
 
             services.AddDbContext<BookStoreContext>(options =>
@@ -55,9 +81,9 @@ namespace AliBookStoreApi
                     {
                         ValidateIssuer = true,
                         ValidateAudience = true,
-                        ValidIssuer = Configuration["JTW : ValidIssuer"],
-                        ValidAudience = Configuration["JTW : ValidAudience"],
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JTW : Sercret"]))
+                        ValidIssuer = Configuration["JWT:ValidIssuer"],
+                        ValidAudience = Configuration["JWT:ValidAudience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["JWT:Secret"]))
                     };
                 });
 
